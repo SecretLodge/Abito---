a0e946781747732ad3d180a4b114a6ef6fce53a3
SecretLodge <88157771+SecretLodge@users.noreply.github.com>
2021-08-07 22:57:39 +0500

    Конечная версия

---
+++ Avito/js/main.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 6,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-panel");
+const menuCloseBtn = document.querySelector(".close-menu");
+
+menuBtn.addEventListener("click", () => { 
+    menu.classList.toggle("is-open");
+});
+
+menuCloseBtn.addEventListener("click", () => { 
+    menu.classList.toggle("is-open");
+});
